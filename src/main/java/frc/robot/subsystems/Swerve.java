0b9acbedb83fa32243e4b4b593bd95b3ec57f08d
swalha1999 <mohamd514.m@gmail.com>
2024-03-03 18:10:21 +0200

    added back the auto for pathpalanner

--- src/main/java/frc/robot/subsystems/Swerve.java
+++ src/main/java/frc/robot/subsystems/Swerve.java
@@ -24,10 +24,10 @@ import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-// import com.pathplanner.lib.auto.AutoBuilder;
-// import com.pathplanner.lib.util.HolonomicPathFollowerConfig;
-// import com.pathplanner.lib.util.PIDConstants;
-// import com.pathplanner.lib.util.ReplanningConfig;
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.util.HolonomicPathFollowerConfig;
+import com.pathplanner.lib.util.PIDConstants;
+import com.pathplanner.lib.util.ReplanningConfig;
 
 public class Swerve extends SubsystemBase {
 	public SwerveDrivePoseEstimator vissionPoseEstimator;
@@ -71,33 +71,33 @@ public class Swerve extends SubsystemBase {
 		);
 
 
-		// AutoBuilder.configureHolonomic(
-		// 		this::getPose, // Robot pose supplier
-		// 		this::setPose, // Method to reset odometry (will be called if your auto has a starting pose)
-		// 		this::getRobotRelativeSpeeds, // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
-		// 		this::driveRobotRelative, // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds
-		// 		new HolonomicPathFollowerConfig( // HolonomicPathFollowerConfig, this should likely live in your
-		// 										 // Constants class
-		// 				new PIDConstants(15.0, 0.0, 0.0), // Translation PID constants
-		// 				new PIDConstants(15.0, 0.0, 0.0), // Rotation PID constants
-		// 				3, // Max module speed, in m/s
-		// 				0.3433, // Drive base radius in meters. Distance from robot center to furthest module.
-		// 				new ReplanningConfig() // Default path replanning config. See the API for the options here
-		// 		),
-		// 		() -> {
-		// 			// Boolean supplier that controls when the path will be mirrored for the red
-		// 			// alliance
-		// 			// This will flip the path being followed to the red side of the field.
-		// 			// THE ORIGIN WILL REMAIN ON THE BLUE SIDE
+		AutoBuilder.configureHolonomic(
+				this::getPose, // Robot pose supplier
+				this::setPose, // Method to reset odometry (will be called if your auto has a starting pose)
+				this::getRobotRelativeSpeeds, // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
+				this::driveRobotRelative, // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds
+				new HolonomicPathFollowerConfig( // HolonomicPathFollowerConfig, this should likely live in your
+												 // Constants class
+						new PIDConstants(15.0, 0.0, 0.0), // Translation PID constants
+						new PIDConstants(15.0, 0.0, 0.0), // Rotation PID constants
+						3, // Max module speed, in m/s
+						0.3433, // Drive base radius in meters. Distance from robot center to furthest module.
+						new ReplanningConfig() // Default path replanning config. See the API for the options here
+				),
+				() -> {
+					// Boolean supplier that controls when the path will be mirrored for the red
+					// alliance
+					// This will flip the path being followed to the red side of the field.
+					// THE ORIGIN WILL REMAIN ON THE BLUE SIDE
 
-		// 			var alliance = DriverStation.getAlliance();
-		// 			if (alliance.isPresent()) {
-		// 				return alliance.get() == DriverStation.Alliance.Red;
-		// 			}
-		// 			return false;
-		// 		},
-		// 		this // Reference to this subsystem to set requirements
-		// );
+					var alliance = DriverStation.getAlliance();
+					if (alliance.isPresent()) {
+						return alliance.get() == DriverStation.Alliance.Red;
+					}
+					return false;
+				},
+				this // Reference to this subsystem to set requirements
+		);
 
 
 	}
