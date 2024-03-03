7b4ea8fd354d62e167c5380ff5a5885464e43d27
swalha1999 <mohamd514.m@gmail.com>
2024-03-03 18:06:50 +0200

    added the new intake

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,23 +1,22 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkFlex;
-import com.revrobotics.ColorSensorV3;
-
+// import com.revrobotics.Rev2mDistanceSensor;
+// import com.revrobotics.Rev2mDistanceSensor.Port;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.wpilibj.I2C;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj.util.Color;
+// import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 
 public class Intake extends SubsystemBase {
 	private CANSparkFlex intakeMotor;
-	private final I2C.Port i2cPort = I2C.Port.kOnboard;
-
-	private final ColorSensorV3 m_colorSensor = new ColorSensorV3(i2cPort);
-
+	// public Rev2mDistanceSensor distanceSensor;
+	
 	public Intake() {
 		intakeMotor = new CANSparkFlex(60, CANSparkFlex.MotorType.kBrushless);
 		intakeMotor.setInverted(true);
+		// distanceSensor = new Rev2mDistanceSensor(Port.kOnboard);
+		// distanceSensor.setMeasurementPeriod(0);
+		// distanceSensor.setAutomaticMode(true);
 	}
 
 	public void setSpeed(double speed) {
@@ -32,22 +31,16 @@ public class Intake extends SubsystemBase {
 		intakeMotor.set(0);
 	}
 
+	// public boolean isNoteInside(){
+	// 	return distanceSensor.getRange() < 15  && distanceSensor.isRangeValid(); 
+	// }
+
 	@Override
 	public void periodic() {
-	
-		Color detectedColor = m_colorSensor.getColor();
-
-		double IR = m_colorSensor.getIR();
-		
-		SmartDashboard.putNumber("Red", detectedColor.red);
-		SmartDashboard.putNumber("Green", detectedColor.green);
-		SmartDashboard.putNumber("Blue", detectedColor.blue);
-		SmartDashboard.putNumber("IR", IR);
-		
-		int proximity = m_colorSensor.getProximity();
-		
-		SmartDashboard.putNumber("Proximity", proximity);
-
+			// distanceSensor.setAutomaticMode(true);
+			// SmartDashboard.putBoolean("Range Valid", distanceSensor.isRangeValid());
+			// SmartDashboard.putNumber("Range Onboard", distanceSensor.getRange());
+			// SmartDashboard.putNumber("Timestamp Onboard", distanceSensor.getTimestamp());
 	}
 
 }
